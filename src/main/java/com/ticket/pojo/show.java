1f395e93841c05f6b4d941227e93dfcd95aaa142
qinshihuang <1766241225@qq.com>
2020-12-02 10:49:20 +0800

    订单，演出实体类创建成功

---
+++ src/main/java/com/ticket/pojo/show.java
@@ -0,0 +1,29 @@
+package com.ticket.pojo;
+
+
+import javax.xml.crypto.Data;
+
+/**
+ * 没有撤退可言
+ **/
+public class show {
+    //演出id
+    private Integer show_id;
+//    演出名
+    private String show_name;
+//    演出类型id
+    private Integer categorie_id;
+//    演出地区
+    private Integer city_id;
+//    演出时间
+    private Data show_time;
+//    演出热度
+    private Integer show_hot;
+//    演出状态
+    private Integer show_state;
+//    演出场次
+    private String show_count;
+//    剩余票数
+    private Integer less_tickets;
+
+}
